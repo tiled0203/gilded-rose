c301b17f1edc05dac1c6b796e063341e94806827
Timothy <tiled0203@gmail.com>
2025-02-13 18:49:52 +0100

    created tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,50 @@
 package com.gildedrose;
 
+import com.gildedrose.domain.*;
+import com.gildedrose.repository.ItemRepository;
+import com.gildedrose.service.GildedRoseBookkeeperService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+    private GildedRoseBookkeeperService gildedRoseBookkeeperService = new GildedRoseBookkeeperService();
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    @BeforeAll
+    static void setUp() {
+        List<Item> items = Arrays.asList(
+            new Common("+5 Dexterity Vest", 10, 20),
+            new Cheese("Aged Brie", 2, 0),
+            new Common("Elixir of the Mongoose", 5, 7),
+            new Legendary("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Legendary("Sulfuras, Hand of Ragnaros", -1, 80),
+            new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Conjured("Conjured Mana Cake", 3, 6));
+        ItemRepository.getInstance().setItems(items);
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1, delimiter = ';')
+    void testGilderRoseCommonItems(int index, String name, int sellIn, int quality) {
+        List<Item> items = gildedRoseBookkeeperService.getItems();
+        assertAll(
+            () -> assertEquals(name, items.get(index).getName()),
+            () -> assertEquals(sellIn, items.get(index).getSellIn()),
+            () -> assertEquals(quality, items.get(index).getQuality())
+        );
+        if (index >= items.size() -1) {
+            gildedRoseBookkeeperService.updateQuality();
+        }
+    }
 }
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,22 +1,30 @@
 package com.gildedrose;
 
+import com.gildedrose.domain.*;
+import com.gildedrose.repository.ItemRepository;
+import com.gildedrose.service.GildedRoseBookkeeperService;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class TexttestFixture {
     public static void main(String[] args) {
+        GildedRoseBookkeeperService gildedRoseBookkeeperService = new GildedRoseBookkeeperService();
+
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
-
-        GildedRose app = new GildedRose(items);
+        List<Item> items = Arrays.asList(
+            new Common("+5 Dexterity Vest", 10, 20),
+            new Cheese("Aged Brie", 2, 0),
+            new Common("Elixir of the Mongoose", 5, 7),
+            new Legendary("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Legendary("Sulfuras, Hand of Ragnaros", -1, 80),
+            new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Conjured("Conjured Mana Cake", 3, 6));
+        ItemRepository.getInstance().setItems(items);
 
         int days = 2;
         if (args.length > 0) {
@@ -30,7 +38,7 @@ public class TexttestFixture {
                 System.out.println(item);
             }
             System.out.println();
-            app.updateQuality();
+            gildedRoseBookkeeperService.updateQuality();
         }
     }
 
